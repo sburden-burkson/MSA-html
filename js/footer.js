981889bebc2cb78c65fe7bdfe2a1594ba12f9bf5
Stephen Burden <spburden@hotmail.com>
2018-02-26 16:49:37 -0800

    footer accordion minus plus logic

--- js/footer.js
+++ js/footer.js
@@ -1,4 +1,4 @@
-function footerAccordion(){
+function footerAccordion() {
   if ($(window).width() > 767) {
     $('[id^="collapse"]').addClass('in');
   } else {
@@ -9,4 +9,10 @@ footerAccordion();
 
 $(window).on('resize', function() {
   footerAccordion();
+});
+
+$('.collapse').on('shown.bs.collapse', function() {
+  $(this).parent().find(".fa-plus").removeClass("fa-plus").addClass("fa-minus");
+}).on('hidden.bs.collapse', function() {
+  $(this).parent().find(".fa-minus").removeClass("fa-minus").addClass("fa-plus");
 });
