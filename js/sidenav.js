76357a9c556930fcb46643d44b29159349353471
Ploki <cschultz@burkson.com>
2018-02-28 09:48:21 -0800

    Sidenav open/close function

---
+++ js/sidenav.js
@@ -0,0 +1,22 @@
+function sidenavTop(){
+	var scrollTop = $(window).scrollTop();
+	var headerHeight = $('header').outerHeight();
+	var newTop = 0;
+	if(scrollTop < headerHeight){
+		newTop = headerHeight - scrollTop;
+	}
+	$('#sidenav .toggle-wrapper').css({'top':newTop+'px'});
+}
+
+$(function(){
+	$('nav .navbar-right .side-nav-open, #sidenav .side-nav-close').click(function(){
+		$('#sidenav').stop().toggleClass('active').removeClass('animated');
+		if($('#sidenav').hasClass('active') && !$('#sidenav').hasClass('animated')){
+			$('#sidenav').addClass('animated').css({'left': '10%'}).animate({'left': '0'}, 300);
+		}
+	});
+	$(window).on('scroll', function(){
+		sidenavTop();
+	});
+	sidenavTop();
+});
