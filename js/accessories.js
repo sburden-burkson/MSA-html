26d7cf981d0bf779ef1146e71ff7f7eaaa971458
Stephen Burden <spburden@hotmail.com>
2018-02-28 12:51:53 -0800

    finish up accessories page

--- js/accessories.js
+++ js/accessories.js
@@ -1,11 +1,11 @@
-var options = {
+var accessoryPageOptions = {
   byRow: false,
-  property: 'height',
-  target: null,
-  remove: false
+  // property: 'height',
+  // target: null,
+  // remove: false
 }
-$(".accessoryHeight1").matchHeight(options);
-$(".accessoryHeight2").matchHeight(options);
-$(".accessoryHeight3").matchHeight(options);
-$(".accessoryHeight4").matchHeight(options);
-$(".accessoryHeight5").matchHeight(options);
+$(".accessoryHeight1").matchHeight(accessoryPageOptions);
+$(".accessoryHeight2").matchHeight(accessoryPageOptions);
+$(".accessoryHeight3").matchHeight(accessoryPageOptions);
+$(".accessoryHeight4").matchHeight(accessoryPageOptions);
+$(".accessoryHeight5").matchHeight();
