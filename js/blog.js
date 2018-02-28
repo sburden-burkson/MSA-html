351e7b11ab9082b077ec5554fcf317f0353bbb04
Stephen Burden <spburden@hotmail.com>
2018-02-28 15:45:29 -0800

    change blog js

--- js/blog.js
+++ js/blog.js
@@ -1,39 +1,8 @@
 var blogItemOptions = {
-  byRow: false,
-  // property: 'min-height',
-  // target: null,
-  // remove: false
+  byRow: false
 }
 $(".featuredBlogHeight").matchHeight();
-$(".blog-item-image-wrap").matchHeight(blogItemOptions);
 $(".blog-item-info-wrap").matchHeight(blogItemOptions);
-$(".blog-item").matchHeight(blogItemOptions);
-
-// Featured Blog Post Sizing
-// function resizeFeatured() {
-//   var padding = parseInt($('#featuredBlog').css('padding-top')) + parseInt($('#featuredBlog').css('padding-bottom'));
-//   var rightH = $('#featuredBlogText').outerHeight();
-//   if ($(window).width() > 767) {
-//     var rightH = $('#featuredBlogText').outerHeight();
-//     $('#featuredBlog').css({
-//       'height': rightH + padding
-//     });
-//     $('#featuredBlogImage').css({
-//       'height': rightH
-//     });
-//   } else {
-//     $('#featuredBlog').css({
-//       'height': "auto"
-//     });
-//     $('#featuredBlogImage').css({
-//       'height': "auto"
-//     });
-//   }
-// }
-// resizeFeatured();
-// $(window).on('resize', function() {
-//   resizeFeatured();
-// });
 
 // Blog Item Masonry
 var $blogGrid = $('.blog-item-container').masonry({
