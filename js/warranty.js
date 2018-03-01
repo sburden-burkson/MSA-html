2bce8190e1ef9578bce688f91d091573f86d7021
Stephen Burden <spburden@hotmail.com>
2018-03-01 13:40:08 -0800

    finish up warranty page

--- js/warranty.js
+++ js/warranty.js
@@ -7,7 +7,22 @@ var warrantyPageOptions = {
 $(".warrantyHeight").matchHeight();
 $(".warrantyOptionHeight").matchHeight();
 $(".tab-pane").matchHeight(false);
-$(".warrantyInfoHeight").matchHeight(false);
+function warrantyInfoCorrectHeight() {
+  if ($(window).width() > 767) {
+    $(".tab-pane").matchHeight(false);
+  } else {
+    // $('.tab-pane').css(
+    //   'height', 'auto'
+    // );
+    $('.tab-pane').matchHeight({ remove: true });
+  }
+}
+warrantyInfoCorrectHeight();
+$(window).on('resize', function() {
+  warrantyInfoCorrectHeight();
+});
+
+// $(".warrantyInfoHeight").matchHeight(false);
 
 $(".warrantyOptionHeight").click(function(){
   $(".warrantyOptionHeight").removeClass("active");
