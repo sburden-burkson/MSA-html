3eb3620d80f7833a5d3ada44ffade601ca0faf63
Stephen Burden <spburden@hotmail.com>
2018-03-02 10:14:44 -0800

    fix image vertical centering on collection pages

--- js/collection.js
+++ js/collection.js
@@ -1,43 +1,3 @@
-var collectionHeightOptions = {
-  byRow: false
-}
-// if ($(window).width() < 480) {
-//   collectionHeightOptions = {
-//     byRow: true
-//   }
-// }
-// Featured Blog Post Sizing
-// function resizeFeatured() {
-//   var padding = parseInt($('#featuredBlog').css('padding-top')) + parseInt($('#featuredBlog').css('padding-bottom'));
-//   var rightH = $('#featuredBlogText').outerHeight();
-//   if ($(window).width() > 767) {
-//     var rightH = $('#featuredBlogText').outerHeight();
-//     $('#featuredBlog').css({
-//       'height': rightH + padding
-//     });
-//     $('#featuredBlogImage').css({
-//       'height': rightH
-//     });
-//   } else {
-//     $('#featuredBlog').css({
-//       'height': "auto"
-//     });
-//     $('#featuredBlogImage').css({
-//       'height': "auto"
-//     });
-//   }
-// }
-// resizeFeatured();
-// $(window).on('resize', function() {
-//   if ($(window).width() < 480) {
-//     collectionHeightOptions = {
-//       byRow: true
-//     }
-//   }
-//   resizeFeatured();
-//   $(".collectionHeight").matchHeight(collectionHeightOptions);
-// });
-
 // Blog Item Masonry
 var $blogGrid = $('.collection-item-container').masonry({
   // options
@@ -51,5 +11,11 @@ $blogGrid.imagesLoaded().progress(function() {
   $blogGrid.masonry('layout');
 });
 
+// Match Height JS
+var collectionHeightOptions = {
+  byRow: false
+}
 $(".collectionHeight").matchHeight(collectionHeightOptions);
 $(".collection-img-wrap").matchHeight(collectionHeightOptions);
+
+$(".collection-col").matchHeight(collectionHeightOptions);
