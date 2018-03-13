acd675822978098b0324f0f1abded2a2a94e7ca8
Ploki <cschultz@burkson.com>
2018-03-13 08:30:55 -0700

    CSS Cleanup and small fixes

--- js/collection.js
+++ js/collection.js
@@ -1,21 +1,23 @@
-// Blog Item Masonry
-var $blogGrid = $('.collection-item-container').masonry({
-  // options
-  itemSelector: '.collection-item',
-  columnWidth: 300,
-  isFitWidth: true,
-  gutter: 20,
-  horizontalOrder: true
-});
-$blogGrid.imagesLoaded().progress(function() {
-  $blogGrid.masonry('layout');
-});
+$(function(){
+    // Blog Item Masonry
+    var $blogGrid = $('.collection-item-container').masonry({
+      // options
+      itemSelector: '.collection-item',
+      columnWidth: 300,
+      isFitWidth: true,
+      gutter: 20,
+      horizontalOrder: true
+    });
+    $blogGrid.imagesLoaded().progress(function() {
+      $blogGrid.masonry('layout');
+    });
 
-// Match Height JS
-var collectionHeightOptions = {
-  byRow: false
-}
-$(".collectionHeight").matchHeight(collectionHeightOptions);
-$(".collection-img-wrap").matchHeight(collectionHeightOptions);
+    // Match Height JS
+    var collectionHeightOptions = {
+      byRow: false
+    }
+    $(".collectionHeight").matchHeight(collectionHeightOptions);
+    $(".collection-img-wrap").matchHeight(collectionHeightOptions);
 
-$(".collection-col").matchHeight(collectionHeightOptions);
+    $(".collection-col").matchHeight(collectionHeightOptions);
+});
