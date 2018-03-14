65c23126f6c7c0c520f683012adc82513bf14b27
Ploki <cschultz@burkson.com>
2018-03-14 16:44:52 -0700

    Updated styles and collection JS for Shop pages

--- js/collection.js
+++ js/collection.js
@@ -1,17 +1,28 @@
-// Collection Masonry
-var $collectionGrid = $('.collection-item-container').masonry({
-  // options
-  itemSelector: '.collection-item',
-  columnWidth: 300,
-  isFitWidth: true,
-  gutter: 20,
-  horizontalOrder: true
-});
-$collectionGrid.imagesLoaded().progress(function() {
-  $collectionGrid.masonry('layout');
-});
+$(function(){
+    // Collection Masonry
+    var $collectionGrid = $('.collection-item-container').masonry({
+      // options
+      itemSelector: '.collection-item',
+      columnWidth: 300,
+      isFitWidth: true,
+      gutter: 20,
+      horizontalOrder: true
+    });
+    $collectionGrid.imagesLoaded().progress(function() {
+      $collectionGrid.masonry('layout');
+    });
 
-// Match Height JS
-$(".collectionHeight").matchHeight({byRow: false});
-$(".collection-img-wrap").matchHeight({byRow: false});
-$(".collection-col").matchHeight({byRow: false});
+    // Match Height JS
+    $(".collectionHeight").matchHeight({byRow: false});
+    $(".collection-img-wrap").matchHeight({byRow: false});
+    $(".collection-col").matchHeight({byRow: false});
+    
+    // Color fix
+    // use var `colorVals` and `queryString` from archive-product.php
+    console.log(queryString);
+    console.log(colorVals);
+    $('.shop-filters-widget-area .woocommerce-widget-layered-nav-list li a').each(function(){
+        var isChosen = ($(this).closest('li').hasClass('chosen'))? true : false;
+        
+    });
+});
